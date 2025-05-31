cdcf2a11b6613604441d91acc623bb5ec660662d
Jasper Fernandez <jasperfernandez148@gmail.com>
2025-05-31 17:58:44 +0800

    chore: cleanup

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Events\TestEvent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/test', function () {
-    TestEvent::dispatch();
-    return 'Event fired';
 });
